4e381f58f923e9f7a14cc55b81de8ec12c001161
qqniqq <st50@WS50>
2026-09-15 12:50:00 +0300

    Задача 1 выполнена

---
+++ Новая папка/150926/150926/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("150926")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("150926")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9820fb14-16a9-4d81-83b7-cce686bf24be")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Новая папка/150926/ClassLibrary/UsersService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class UsersService
+    {
+        IUsersRepository repository_;
+        public UsersService(IUsersRepository repository)
+        {
+            repository_ = repository;
+        }
+        public string Autorization(string login, string password)
+        {
+            string result = "ERORE";
+            User user = repository_.GetUser(login);
+            if (user.Password == password)
+            { result = "true"; }
+            return result;
+        }
+    }
+}
