ddffd8e5a3b022c801c9221d5d28242f4903afcc
qqniqq <st50@WS50>
2026-09-15 13:20:15 +0300

    Пара закончена

---
+++ Новая папка/150926/150926/UsersServiseTest.cs
@@ -0,0 +1,30 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Moq;
+using System.Security.Cryptography.X509Certificates;
+
+namespace _150926
+{
+    [TestClass]
+    public class UsersServiseTest
+    {
+        
+        [TestMethod]
+        public void TAutorization()
+        {
+            var mock = new Mock<IUsersRepository>();
+            mock.Setup(repo => repo.GetUser("login"))
+                .Returns(new User { Login = "login", Password = "pass" });
+            var service = new UsersService(mock.Object);
+            string login = "login";
+            string password = "pass";
+
+            string expected = "true";
+
+            string actual = service.Autorization(login, password);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
--- Новая папка/150926/ClassLibrary/User.cs
+++ Новая папка/150926/ClassLibrary/User.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ClassLibrary
 {
-    internal class User
+    public class User
     {
         public string Login {get; set;}
         public string Password {get; set;} 
